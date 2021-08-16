b603821b2aef2f320a70e758876cbcc6219ed52a
masaru-ichikawa <masaru-ichikawa@cookpad.com>
2021-08-17 06:15:34 +0900

    removed needless code

--- MiniMart/ContentView.swift
+++ MiniMart/ContentView.swift
@@ -6,41 +6,6 @@ struct ContentView: View {
     }
 }
 
-
-struct MyView: View {
-    @State var isLoading: Bool = false
-    @State var jankenResult: String?
-    @State var error: String?
-    var body: some View {
-        VStack {
-            if isLoading {
-                Text("読込中")
-            } else {
-                if let error = error {
-                    Text("問題が発生しました: \(error))")
-                } else if let jankenResult = jankenResult {
-                    switch jankenResult {
-                        case "win":
-                            Text("勝ち")
-                        case "lose":
-                            Text("負け")
-                        case "even":
-                            Text("あいこ")
-                        default:
-                            Text("不明")
-                    }
-                } else {
-                    Text("不明")
-                }
-            }
-            Button(action: { /* JankenProvider.execute { self.jankenResult = $0 } */ }) {
-                Text("じゃんけんをする")
-            }
-        }
-    }
-}
-
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
