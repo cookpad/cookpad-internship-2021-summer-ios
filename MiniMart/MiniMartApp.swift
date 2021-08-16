02b47824244b2f8527c5e83fd82b3a3e4f37e953
masaru-ichikawa <masaru-ichikawa@cookpad.com>
2021-08-16 18:59:33 +0900

    wrap with navigation view

--- MiniMart/MiniMartApp.swift
+++ MiniMart/MiniMartApp.swift
@@ -4,7 +4,9 @@ import SwiftUI
 struct MiniMartApp: App {
     var body: some Scene {
         WindowGroup {
-            ProductListPageView()
+            NavigationView {
+                ProductListPageView()
+            }
         }
     }
 }
